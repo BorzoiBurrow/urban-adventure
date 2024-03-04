7e7b530726830db9d22adac10d97d058c84ae18b
Brad <Bradrockk@gmail.com>
2024-03-04 05:55:38 -0500

    added placeholders in portfolio section

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../styles/styles.css';
 
 const Portfolio = () => {
-    return <h2>Portfolio Section</h2>
+  return (
+    <div className="portfolio-container">
+      <h2>Portfolio Section</h2>
+      <div className="image-row">
+        <img src="Placeholder" alt="Placeholder" />
+        <img src="Placeholder" alt="Placeholder" />
+        <img src="Placeholder" alt="Placeholder" />
+      </div>
+      <div className="image-row">
+        <img src="Placeholder" alt="Placeholder" />
+        <img src="Placeholder" alt="Placeholder" />
+        <img src="Placeholder" alt="Placeholder" />
+      </div>
+    </div>
+  );
 }
 
-export default Portfolio
+export default Portfolio;
