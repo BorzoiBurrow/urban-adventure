bf7c90072513737efd8267056da66ec6ab5754bf
Brad <Bradrockk@gmail.com>
2024-03-05 10:22:56 -0500

    adjusted portfolio styles and replaced some placeholders

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -4,10 +4,17 @@ import '../styles/styles.css';
 const Portfolio = () => {
   return (
     <div className="portfolio-container">
+      <h2>Click to view any of these repositories!</h2>
       <div className="image-row">
-        <img src="Placeholder" alt="Placeholder" />
-        <img src="Placeholder" alt="Placeholder" />
-        <img src="Placeholder" alt="Placeholder" />
+        <a href="https://github.com/BorzoiBurrow/urban-guide">
+        <img src="image/database.png" alt="Placeholder" />
+        </a>
+        <a href="https://github.com/BorzoiBurrow/turbo-spork">
+        <img src="image/commerce.png" alt="Placeholder" />
+        </a>
+        <a href="https://github.com/BorzoiBurrow/fictional-journey">
+        <img src="image/blog.png" alt="Placeholder" />
+        </a>
       </div>
       <div className="image-row">
         <img src="Placeholder" alt="Placeholder" />
