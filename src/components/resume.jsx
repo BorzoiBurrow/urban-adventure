7267b50d97cb368d159093338e7f091f4e1f89d6
Brad <Bradrockk@gmail.com>
2024-03-05 14:37:32 -0500

    adjusted resume section and styles

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -3,9 +3,13 @@ import React from 'react';
 const resume = () => {
 
   return (
+    <div className='resumeText'>
     <a href="/resume">
-      <h2>Click here to download a copy of my resume!</h2>
-    </a>
+      <h3>Click here to download a copy of my resume!</h3>
+      </a>
+      <p> Feel free to contact me with any questions, comments, concerns. </p>
+      <p>I would love to hear your thoughts or recommendations for this site! </p>
+    </div>
   );
 }
 
