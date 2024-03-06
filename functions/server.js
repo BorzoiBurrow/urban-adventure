b98acc585db6bb87827a2fc19e588833837bb401
Brad <Bradrockk@gmail.com>
2024-03-06 06:19:06 -0500

    adjusted server

---
+++ functions/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'src/pages', 'index.html'));
+});
+
+app.get('/dist/bundle.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', '/dist', 'bundle.js'));
+});
+
+app.use('/image', express.static(path.join(__dirname, '..', 'src/images')));
+app.use('/assets', express.static(path.join(__dirname, '..', 'src/assets')));
+
+app.get('/resume', (req, res) => {
+  try {
+    const resumeFileName = 'resume.pdf';
+    const resumePath = path.join(__dirname, '..', 'src/assets', resumeFileName);
+
+    res.download(resumePath, resumeFileName);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports.handler = serverless(app);
