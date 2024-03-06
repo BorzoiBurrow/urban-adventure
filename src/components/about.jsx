756fcfa69c5a1435a3beb086912e00c6985a9236
Brad <Bradrockk@gmail.com>
2024-03-06 07:07:43 -0500

    adjusted about path for testing

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,7 +5,7 @@ import '../styles/styles.css';
 const about = () => {
     return (
       <div className="content-container">
-        <img src="avatar.png" alt="Placeholder" />
+        <img src="./assets/avatar.png" alt="Placeholder" />
         <p>Hello, and welcome! My name is Bradley, and i'm currently enrolled in a full-stack development course at UPENN. This site was made as a short Demo of some of my projects.</p>
       </div>
     );
